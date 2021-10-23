a70616d0fde59a4b4c51c856f0088127208515ba
Gabriel Birke <gabriel.birke@gmail.com>
2021-10-23 10:36:45 +0200

    Add PSR12 coding style and check it

--- public/index.php
+++ public/index.php
@@ -14,41 +14,41 @@ $app = AppFactory::create();
 
 // Check Authentication
 $app->get('/auth', function (Request $request, Response $response, $args) {
-	if( !empty($_SESSION['username']) ) {
-		$response->getBody()->write("OK");
-		return $response;
-	}
-	return $response->withStatus(302)
-		->withHeader("Location", "/login");
+    if (!empty($_SESSION['username'])) {
+        $response->getBody()->write("OK");
+        return $response;
+    }
+    return $response->withStatus(302)
+        ->withHeader("Location", "/login");
 });
 
 // Status page
 $app->get('/', function (Request $request, Response $response, $args) {
-	$username = $_SESSION['username'] ?? '';
+    $username = $_SESSION['username'] ?? '';
     $response->getBody()->write($username ? "Logged in as $username" : "You are not logged in");
     return $response;
 });
 
 // Process login form
 $app->post('/login', function (Request $request, Response $response, $args) {
-	$params = (array)$request->getParsedBody();
-	$username = $params['username'] ?? '';
-	$password = $params['password'] ?? '';
-	// TODO avoid hardcoding
-	if ($username !== 'user' || $password !== 'insecure' ) {
-		$response->getBody()->write("Login failed");
-		return $response->withStatus(403);
-	}
-	$_SESSION['username'] = $username;
-	$query = $request->getQueryParams();
-	// TODO redirect with query params, to x-forwarded-host or to FQDN
-	return $response->withStatus(302)
-		->withHeader("Location", "/");
+    $params = (array)$request->getParsedBody();
+    $username = $params['username'] ?? '';
+    $password = $params['password'] ?? '';
+    // TODO avoid hardcoding
+    if ($username !== 'user' || $password !== 'insecure') {
+        $response->getBody()->write("Login failed");
+        return $response->withStatus(403);
+    }
+    $_SESSION['username'] = $username;
+    $query = $request->getQueryParams();
+    // TODO redirect with query params, to x-forwarded-host or to FQDN
+    return $response->withStatus(302)
+        ->withHeader("Location", "/");
 });
 
 // Show login form
 $app->get('/login', function (Request $request, Response $response, $args) {
-	$response->getBody()->write(<<<LOGINFORM
+    $response->getBody()->write(<<<LOGINFORM
 		<html>
 			<head><title>Login</title></head>
 			<body>
@@ -60,17 +60,15 @@ $app->get('/login', function (Request $request, Response $response, $args) {
 			</body>
 		</html>
 		LOGINFORM
-	);
+    );
     return $response;
 });
 
 $app->get('/logout', function (Request $request, Response $response, $args) {
-	unset($_SESSION['username']);
-	return $response->withStatus(302)
-		->withHeader("Location", "/");
+    unset($_SESSION['username']);
+    return $response->withStatus(302)
+        ->withHeader("Location", "/");
 });
 
 
 $app->run();
-
-
